7b6a2a918d43b582486dfe5e141ee36478649537
Anshul <ay477510@gmail.com>
2026-06-10 13:20:42 +0000

    Added different typpes of data types to be used in kernel

---
+++ include/types.h
@@ -0,0 +1,13 @@
+cat > ~TinyOS/include/types.h << 'EOF'
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned long   uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef uint32_t size_t;
+
+#endif 
+EOF
