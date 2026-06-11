e72fd0967d85329fdd61b1e9adf189f84224b104
Anshul <ay477510@gmail.com>
2026-06-11 09:23:17 +0000

    Added void screen functions and basic color assign pointers

---
+++ kernel/vga.c
@@ -0,0 +1,31 @@
+#include "vga.h"
+
+static uint16_t* VGA_MEMORY  (uint16_t*) 0xB8000;
+
+static size_t vga_row;
+static size_t vga_col;
+static uint8_t vga_color;
+
+// Pack a foreground + background color into one byte
+// Upper 4 bits = background, lower 4 bits = foreground
+
+static uint8_t make_color(vga_color_t fg, vga_color_t bg) {
+    return fg | bg << 4;
+}
+
+static uint16_t make_vga_entry(char c, uint8_t color) {
+    return (uint16_t) c | (uint16_t) color << 8;
+}
+
+void vga_set_color(vga_color_t fg, vga_color_t bg) {
+    vga_color = make_color(fg, bg);
+}
+
+void vga_clear(void){
+    uint16_t blank = make_vga_entry(' ' , vga_color);
+    for(size_t y = 0 ; y < VGA_HEIGHT * VGA_WIDTH ; y++){
+        VGA_MEMORY[y] = blank ;
+    }
+    vga_row = 0 ;
+    vga_col = 0 ;
+}
