9624410d9e719d1fd26273a42019b030346da0c2
Anshul <ay477510@gmail.com>
2026-06-13 20:04:24 +0000

    Added multiboot header section

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,2 +1,13 @@
 bits 32
-MULTIBOOT_MAGIC equ 0x1BADB002
+MULTIBOOT_MAGIC equ 0x1BADB002 ;
+MULTIBOOT_FLAGS equ 0x00000003 ;
+MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS) ;
+
+; ── Multiboot header section ───────────────────────────────────────────────────
+section .multiboot
+align 4  ;
+dd MULTIBOOT_MAGIC
+dd MULTIBOOT_FLAGS
+dd MULTIBOOT_CHECKSUM
+
+// we reserve 16KB for the bootloader, so that it can load the kernel at 0x100000
