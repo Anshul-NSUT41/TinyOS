345a66cd9fdba4f7bbc45f14ec9fffbbc8599c7b
Anshul <ay477510@gmail.com>
2026-06-13 20:16:55 +0000

    added kernel entry point to the assembly stub

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,11 +1,11 @@
 bits 32
-MULTIBOOT_MAGIC equ 0x1BADB002 ;
-MULTIBOOT_FLAGS equ 0x00000003 ;
-MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS) ;
+MULTIBOOT_MAGIC equ 0x1BADB002 
+MULTIBOOT_FLAGS equ 0x00000003 
+MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS) 
 
 ; ── Multiboot header section ───────────────────────────────────────────────────
 section .multiboot
-align 4  ;
+align 4  
 dd MULTIBOOT_MAGIC
 dd MULTIBOOT_FLAGS
 dd MULTIBOOT_CHECKSUM
@@ -16,3 +16,17 @@ dd MULTIBOOT_CHECKSUM
     stack_bottom :
       resb 16384 ; 16KB stack
     stack_top :
+
+; ── Kernel Entry Point ───────────────────────────────────────────────────
+section .text
+global _start 
+extern kernel_main
+
+_start:
+  mov esp , stack_top 
+  call kernel_main 
+  
+.hang:
+   cli  
+   hlt
+   jmp.hang
