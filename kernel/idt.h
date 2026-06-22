795cde9bb7303c225a6eddefc86ae18cb52ad1b2
Anshul <ay477510@gmail.com>
2026-06-22 18:54:43 +0000

    feat : Add idt_ptr struct

--- kernel/idt.h
+++ kernel/idt.h
@@ -9,3 +9,8 @@ typedef struct __attribute__((packed)) {
     uint8_t  flags;     
     uint16_t base_high;   
 } idt_entry_t;
+
+typedef struct __attribute__((packed)) {
+    uint16_t limit;      
+    uint32_t base;        
+} idt_ptr_t;
