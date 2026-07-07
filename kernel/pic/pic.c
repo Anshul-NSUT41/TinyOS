8b7558f6077d90d0562de0fc98561cbd139fccef
Anshul <ay477510@gmail.com>
2026-07-07 07:37:21 +0000

    feat : mapped different i/o handlers to multiple port numbers

--- kernel/pic/pic.c
+++ kernel/pic/pic.c
@@ -2,5 +2,26 @@
 #include "ports.h"
 
 void pic_init(void){
-    
+    port_byte_out(PIC1_COMMAND, 0x11);  // 0x11 = init + ICW4 needed
+    port_byte_out(PIC2_COMMAND, 0x11);
+    io_wait();
+
+
+    port_byte_out(PIC1_DATA, 0x20);
+    port_byte_out(PIC2_DATA, 0x28);
+    io_wait();
+
+    port_byte_out(PIC1_DATA, 0x01);
+    port_byte_out(PIC2_DATA, 0x01);
+    io_wait();
+
+    port_byte_out(PIC1_DATA, 0xFD);
+    port_byte_out(PIC2_DATA, 0xFF);  // Mask all slave IRQs
+}
+
+void pic_send_eoi(uint8_t irq){
+    if(irq >= 8){
+        port_byte_out(PIC2_COMMAND, PIC_EOI);  // Send EOI to slave PIC
+    }
+    port_byte_out(PIC1_COMMAND, PIC_EOI);  // Send EOI to master PIC
 }
