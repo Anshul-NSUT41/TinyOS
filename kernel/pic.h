d836a3bf20b8c6452092f73442425097f7ec9429
Anshul <ay477510@gmail.com>
2026-07-07 07:11:07 +0000

    Add data , and command ports

--- kernel/pic.h
+++ kernel/pic.h
@@ -12,3 +12,8 @@
 //   IRQ2–7           → interrupts 34–39
 //   IRQ8–15          → interrupts 40–47
 
+#define PIC1_COMMAND 0x20   // Master PIC command port
+#define PIC1_DATA    0x21   // Master PIC data port
+#define PIC2_COMMAND 0xA0   // Slave PIC command port
+#define PIC2_DATA    0xA1   // Slave PIC data port
+#define PIC_EOI      0x20   // End-of-interrupt command
