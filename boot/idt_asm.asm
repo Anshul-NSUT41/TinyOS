8b245f7cb93031d2ab5c28de6ec5c9ad9c2d293d
Anshul <ay477510@gmail.com>
2026-07-07 06:18:11 +0000

    feat:  Added idt_asm assembly stub for loading idt script

---
+++ boot/idt_asm.asm
@@ -0,0 +1,12 @@
+bits 32
+section .text
+
+global idt_load ; 
+
+; void idt_load(uint32_t idt_ptr_address);
+; The argument arrives on stack at [esp + 4]
+
+idt_load:
+    mov eax, [esp + 4] ; Load the address of the IDT pointer into EAX
+    lidt [eax]         ; Load the IDT using the lidt instruction
+    ret                ; Return from the function
--- kernel/idt.h
+++ kernel/idt.h
@@ -14,3 +14,8 @@ typedef struct __attribute__((packed)) {
     uint16_t limit;      
     uint32_t base;        
 } idt_ptr_t;
+
+void idt_init(void);
+void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
+
+#endif
