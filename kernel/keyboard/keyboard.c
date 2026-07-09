2bd8286c83fcd56e978d06bc7eb6b295e040c289
Anshul <ay477510@gmail.com>
2026-07-09 11:59:19 +0000

    Added the keyboard handler function

--- kernel/keyboard/keyboard.c
+++ kernel/keyboard/keyboard.c
@@ -39,3 +39,36 @@ static const char scancode_to_ascii_shift[] = {
     0, '*', 0, ' ',
 };
 
+#define KEY_BUFFER_SIZE 256
+static char key_buffer[KEY_BUFFER_SIZE];
+static volatile uint8_t buf_head = 0;  // next write position
+static volatile uint8_t buf_tail = 0;  // next read position
+
+static void keyboard_handler_impl(void) {
+    uint8_t scancode = port_byte_in(0x60);  
+    // Bit 7 of scancode = key release (1) vs key press (0)
+    uint8_t released = scancode & 0x80;
+    uint8_t key      = scancode & 0x7F;  
+    if (key == 0x2A || key == 0x36) {    
+        shift_pressed = !released;
+        pic_send_eoi(1);
+        return;
+    }
+    if (released) {
+        pic_send_eoi(1);
+        return;
+    }
+    char c = 0;
+    if (key < SCANCODE_TABLE_SIZE) {
+        c = shift_pressed ? scancode_to_ascii_shift[key]
+                          : scancode_to_ascii[key];
+    }
+    if (c != 0) {
+        uint8_t next = (buf_head + 1) % KEY_BUFFER_SIZE;
+        if (next != buf_tail) { 
+            key_buffer[buf_head] = c;
+            buf_head = next;
+        }
+    }
+    pic_send_eoi(1); 
+}
